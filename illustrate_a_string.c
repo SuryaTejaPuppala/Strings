5cd6e988a4327993d82766e5befd0cc19e3f0d77
SuryaTejaPuppala <136901713+SuryaTejaPuppala@users.noreply.github.com>
2023-06-26 15:37:28 +0530

    illustrate_a_string.c

---
+++ illustrate_a_string.c
@@ -0,0 +1,11 @@
+//Illustrate a String
+#include<stdio.h>
+void main()
+{
+	char str[6]="Hello";
+	char month[]="January";
+	char city[8]={'n','e','w','y','o','r','k'};
+	printf("str = %s",str);
+	printf("\nmonth= %s",month);
+	printf("\ncity = %s",city);
+}
